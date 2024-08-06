552d08fd5a2f5a448429997fae0f2f5044454e5c
stephen_adah <mailstephenreal@gmail.com>
2024-08-06 08:59:05 +0100

    Remove config:Not neccessary:Done in __init__.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 """module for flask application"""
 from app import create_app
-from flask_mail import Mail
 
 app = create_app()
-app.config.from_object('app.config.Config')
-mail = Mail()
 
 
 if __name__ == "__main__":
